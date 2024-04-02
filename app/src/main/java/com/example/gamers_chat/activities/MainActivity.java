ee2df47dedcff1f3583613daba9ace6ee893b576
TheLordPasta <arielshirkanistudent@gmail.com>
2024-04-02 22:06:15 +0300

    added new shit

--- app/src/main/java/com/example/gamers_chat/activities/MainActivity.java
+++ app/src/main/java/com/example/gamers_chat/activities/MainActivity.java
@@ -2,20 +2,34 @@ package com.example.gamers_chat.activities;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
 
+import com.bumptech.glide.Glide;
 import com.example.gamers_chat.R;
 import com.example.gamers_chat.models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,10 +38,29 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-
+    StorageReference storageRef;
+    LinearProgressIndicator progress;
+    Uri image;
+    MaterialButton selectImage, uploadImage;
+    ImageView imageView;
+    private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == RESULT_OK) {
+                if (result.getData() != null) {
+                    image = result.getData().getData();
+                    uploadImage.setEnabled(true);
+                    Glide.with(getApplicationContext()).load(image).into(imageView);
+                }
+            } else {
+                Toast.makeText(MainActivity.this, "Please select an image", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
     public User currentUser;
 
 
@@ -35,8 +68,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        FirebaseApp.initializeApp(MainActivity.this);
         mAuth = FirebaseAuth.getInstance();
     }
     public void registerUserData() {
